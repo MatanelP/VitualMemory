219b6b95442829dc18edebb1e6102371cfd785e6
MatanelP <75391764+MatanelP@users.noreply.github.com>
2022-05-19 20:23:13 +0300

    implemented VMinitialize and 2 helper-functions for extracting required bits

--- VirtualMemory.cpp
+++ VirtualMemory.cpp
@@ -1,11 +1,34 @@
 #include "VirtualMemory.h"
+#include "PhysicalMemory.h"
 
 /*
  * Initialize the virtual memory.
  */
 void VMinitialize(){
-
+  //  clearing frame 0:
+  for(uint64_t i = 0 ; i < PAGE_SIZE ; i++)
+    PMwrite (i, 0);
 }
+
+/*
+ * extracting the offset from the given logical address
+ */
+uint64_t getOffset(uint64_t virtualAddress){
+  return ((1 << OFFSET_WIDTH) - 1) & virtualAddress;
+}
+
+/*
+ * extracting the physical address of the wanted page table
+ * level from the given logical address (the P_i)
+ */
+uint64_t getAddressForLevel(uint64_t virtualAddress, int level){
+    // assert(level < TABLES_DEPTH)
+    if (level == 0){
+        return virtualAddress >> ((TABLES_DEPTH) * OFFSET_WIDTH);
+    }
+    return virtualAddress >> ((TABLES_DEPTH - level) * OFFSET_WIDTH) & ((1 << OFFSET_WIDTH) - 1);
+}
+
 
 /* Reads a word from the given virtual address
  * and puts its content in *value.
@@ -15,6 +38,8 @@ void VMinitialize(){
  * address for any reason)
  */
 int VMread(uint64_t virtualAddress, word_t* value){
+  uint64_t offset = getOffset (virtualAddress);
+
 
   return 0;
 }
