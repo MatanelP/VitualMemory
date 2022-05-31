c0df03b6f20d53002311bdf50a0eb195946f1c88
nadav203 <nadav203@gmail.com>
2022-05-31 18:10:21 +0300

    pass all tests

--- VirtualMemory.cpp
+++ VirtualMemory.cpp
@@ -7,9 +7,9 @@
  */
 void clearFrame (uint64_t frame)
 {
-  for (uint64_t i = 0; i < PAGE_SIZE; i++)
+    for (uint64_t i = 0; i < PAGE_SIZE; i++)
     {
-      PMwrite (frame * PAGE_SIZE + i, 0);
+        PMwrite (frame * PAGE_SIZE + i, 0);
     }
 }
 
@@ -18,7 +18,7 @@ void clearFrame (uint64_t frame)
  */
 void VMinitialize ()
 {
-  clearFrame (0);
+    clearFrame (0);
 }
 
 /**
@@ -28,7 +28,7 @@ void VMinitialize ()
  */
 uint64_t getOffset (uint64_t virtualAddress)
 {
-  return ((1 << OFFSET_WIDTH) - 1) & virtualAddress;
+    return ((1 << OFFSET_WIDTH) - 1) & virtualAddress;
 }
 
 /**
@@ -40,47 +40,47 @@ uint64_t getOffset (uint64_t virtualAddress)
  */
 uint64_t getAddressForLevel (uint64_t virtualAddress, int level)
 {
-  // assert(level < TABLES_DEPTH)
-  if (level == 0)
+    // assert(level < TABLES_DEPTH)
+    if (level == 0)
     {
-      return virtualAddress >> ((TABLES_DEPTH) * OFFSET_WIDTH);
+        return virtualAddress >> ((TABLES_DEPTH) * OFFSET_WIDTH);
     }
-  return virtualAddress >> ((TABLES_DEPTH - level) * OFFSET_WIDTH)
-         & ((1 << OFFSET_WIDTH) - 1);
+    return virtualAddress >> ((TABLES_DEPTH - level) * OFFSET_WIDTH)
+           & ((1 << OFFSET_WIDTH) - 1);
 }
 
 bool isEmptyFrame (uint64_t frame)
 {
-  for (uint64_t i = 0; i < PAGE_SIZE; i++)
+    for (uint64_t i = 0; i < PAGE_SIZE; i++)
     {
-      word_t value = 0;
-      PMread (frame * PAGE_SIZE + i, &value);
-      if (value != 0) return false;
+        word_t value = 0;
+        PMread (frame * PAGE_SIZE + i, &value);
+        if (value != 0) return false;
     }
-  return true;
+    return true;
 }
 
 /**
- * Helper function for case 1 of getFrame
+ * Helper function for case 1 of runDFS
  * Removing references to this table from its parent
  * @param parent
  * @param frame
  */
 void removeReference (uint64_t parent, uint64_t frame)
 {
-  for (uint64_t i = 0; i < PAGE_SIZE; i++)
+    for (uint64_t i = 0; i < PAGE_SIZE; i++)
     {
-      word_t value = 0;
-      PMread (parent * PAGE_SIZE + i, &value);
-      if (value == frame)
+        word_t value = 0;
+        PMread (parent * PAGE_SIZE + i, &value);
+        if (value == frame)
         {
-          PMwrite (parent * PAGE_SIZE + i, 0);
+            PMwrite (parent * PAGE_SIZE + i, 0);
         }
     }
 }
 
 /**
- * Helper function updating the cyclical distance, the max/min formula used in case 3 of getFrame
+ * Helper function updating the cyclical distance, the max/min formula used in case 3 of runDFS
  * @param virtualPageNum
  * @param page
  * @param currentCyclicalDistance
@@ -88,11 +88,11 @@ void removeReference (uint64_t parent, uint64_t frame)
 void
 updateMaxCyclical (uint64_t virtualPageNum, uint64_t page, uint64_t *currentCyclicalDistance)
 {
-  uint64_t d = virtualPageNum > page ?
-               virtualPageNum - page : page - virtualPageNum;
-  int cyclicalDistance = NUM_PAGES - d > d ? d : NUM_PAGES - d;
-  *currentCyclicalDistance = cyclicalDistance > *currentCyclicalDistance ?
-                             cyclicalDistance : *currentCyclicalDistance;
+    uint64_t d = virtualPageNum > page ?
+                 virtualPageNum - page : page - virtualPageNum;
+    int cyclicalDistance = NUM_PAGES - d > d ? d : NUM_PAGES - d;
+    *currentCyclicalDistance = cyclicalDistance > *currentCyclicalDistance ?
+                               cyclicalDistance : *currentCyclicalDistance;
 }
 
 /**
@@ -114,65 +114,94 @@ updateMaxCyclical (uint64_t virtualPageNum, uint64_t page, uint64_t *currentCycl
  * @param pageToEvict
  */
 void
-getFrame (uint64_t virtualPageNum, uint64_t frame, uint64_t parent,
-          uint64_t *maxFrameNum, int level, uint64_t *availableFrame,
-          uint64_t *evictPageFromFrameNum, uint64_t *parentOfEvictPageFromFrameNum,
-          uint64_t currentCyclicalDistance, uint64_t page, uint64_t unavailableFrame, uint64_t *pageToEvict)
+runDFS (uint64_t virtualPageNum, uint64_t frame, uint64_t parent,
+        uint64_t *maxFrameNum, int level, uint64_t *availableFrame,
+        uint64_t *evictPageFromFrameNum, uint64_t *parentOfEvictPageFromFrameNum,
+        uint64_t currentCyclicalDistance, uint64_t page, uint64_t unavailableFrame, uint64_t *pageToEvict)
 {
-  if (frame != 0)
+    if (frame != 0)
     {
-      //3. calc minimum cyclical distance and update maximum:
-      if (level == TABLES_DEPTH)
+        //3. calc minimum cyclical distance and update maximum:
+        if (level == TABLES_DEPTH)
         {
-          uint64_t currentlyDist = currentCyclicalDistance;
-          updateMaxCyclical (virtualPageNum, page, &currentCyclicalDistance);
-          if (currentlyDist != currentCyclicalDistance)
+            uint64_t currentlyDist = currentCyclicalDistance;
+            updateMaxCyclical (virtualPageNum, page, &currentCyclicalDistance);
+            if (currentlyDist != currentCyclicalDistance)
             {
-              *evictPageFromFrameNum = frame;
-              *parentOfEvictPageFromFrameNum = parent;
+                *evictPageFromFrameNum = frame;
+                *parentOfEvictPageFromFrameNum = parent;
             }
-          *pageToEvict = page;
+            *pageToEvict = page;
         }
 
 
-      // 1. check for empty table:
-      if (isEmptyFrame (frame) && level != TABLES_DEPTH
-          && frame != unavailableFrame)
+        // 1. check for empty table:
+        if (isEmptyFrame (frame) && level != TABLES_DEPTH
+            && frame != unavailableFrame)
         { //all rows are 0's
-          //  remove reference from parent
-          removeReference (parent, frame);
-          *availableFrame = frame;
-          *maxFrameNum = 0;
-          return;
+            //  remove reference from parent
+            removeReference (parent, frame);
+            *availableFrame = frame;
+            *maxFrameNum = 0;
+            return;
         }
 
     }
 
-  // going through the current frame's children and recurse if needed
-  for (uint64_t row = 0; row < PAGE_SIZE; row++)
+    // going through the current frame's children and recurse if needed
+    for (uint64_t row = 0; row < PAGE_SIZE; row++)
     {
-      word_t child = 0;
-      if (level != TABLES_DEPTH)
+        word_t child = 0;
+        if (level != TABLES_DEPTH)
         {
-          PMread (frame * PAGE_SIZE + row, &child);
+            PMread (frame * PAGE_SIZE + row, &child);
 
-          if (child != 0)
+            if (child != 0)
             {
-              *maxFrameNum = *maxFrameNum > child ? *maxFrameNum : child;
+                *maxFrameNum = *maxFrameNum > child ? *maxFrameNum : child;
 
-              getFrame (virtualPageNum, child, frame,
-                        maxFrameNum, level + 1, availableFrame,
-                        evictPageFromFrameNum, parentOfEvictPageFromFrameNum,
-                        currentCyclicalDistance, (page << OFFSET_WIDTH)
-                                                 + row, unavailableFrame, pageToEvict);
+                runDFS(virtualPageNum, child, frame,
+                       maxFrameNum, level + 1, availableFrame,
+                       evictPageFromFrameNum, parentOfEvictPageFromFrameNum,
+                       currentCyclicalDistance, (page << OFFSET_WIDTH)
+                                                + row, unavailableFrame, pageToEvict);
 
-              if (*availableFrame != 0) //found availableFrame
+                if (*availableFrame != 0) //found availableFrame
                 {
-                  return;
+                    return;
                 }
             }
         }
     }
+
+}
+
+uint64_t getFrame(uint64_t virtualPageNum, word_t oldAddress){
+
+    uint64_t maxFrameNum = 0;
+    uint64_t availableFrame = 0;
+    uint64_t evictPageFromFrameNum = 0;
+    uint64_t parentOfEvictPageFromFrameNum = 0;
+    uint64_t pageToEvict = 0;
+    runDFS(virtualPageNum, 0, 0, &maxFrameNum,
+           0, &availableFrame, &evictPageFromFrameNum,
+           &parentOfEvictPageFromFrameNum, 0, 0,
+           oldAddress, &pageToEvict);
+    if (availableFrame == 0 || availableFrame == oldAddress)
+    {
+        availableFrame = maxFrameNum + 1;
+    }
+
+
+    if (maxFrameNum >= NUM_FRAMES - 1)
+    {
+        PMevict (evictPageFromFrameNum, pageToEvict);
+        removeReference (parentOfEvictPageFromFrameNum, evictPageFromFrameNum);
+        availableFrame = evictPageFromFrameNum;
+    }
+
+    return availableFrame;
+
 
 }
 
@@ -183,50 +212,32 @@ getFrame (uint64_t virtualPageNum, uint64_t frame, uint64_t parent,
  */
 word_t getAddress (uint64_t virtualAddress)
 {
-  uint64_t virtualPageNum = virtualAddress >> OFFSET_WIDTH;
-  word_t curAddress = 0;
-  word_t oldAddress = 0;
-  for (int i = 0; i < TABLES_DEPTH; ++i)
+    uint64_t virtualPageNum = virtualAddress >> OFFSET_WIDTH;
+    word_t curAddress = 0;
+    word_t oldAddress = 0;
+    uint64_t availableFrame;
+    for (int i = 0; i < TABLES_DEPTH; ++i)
     {
-      oldAddress = curAddress;
-      uint64_t levelOffset = getAddressForLevel (virtualAddress, i);
-      PMread (oldAddress * PAGE_SIZE + levelOffset, &curAddress);
-      if (curAddress == 0)
+        oldAddress = curAddress;
+        uint64_t levelOffset = getAddressForLevel (virtualAddress, i);
+        PMread (oldAddress * PAGE_SIZE + levelOffset, &curAddress);
+        if (curAddress == 0)
         {
-          uint64_t availableFrame = 0;
-          uint64_t maxFrameNum = 0;
-          uint64_t evictPageFromFrameNum = 0;
-          uint64_t parentOfEvictPageFromFrameNum = 0;
-          uint64_t pageToEvict = 0;
-          getFrame (virtualPageNum, 0, 0, &maxFrameNum,
-                    0, &availableFrame, &evictPageFromFrameNum,
-                    &parentOfEvictPageFromFrameNum, 0, 0,
-                    oldAddress, &pageToEvict);
-          if (availableFrame == 0 || availableFrame == oldAddress)
-            {
-              availableFrame = maxFrameNum + 1;
-            }
-          else if (i < TABLES_DEPTH - 1)
+            availableFrame = getFrame(virtualPageNum, oldAddress);
+
+            if (i < TABLES_DEPTH - 1)
             {// next layer is a table, need to clear contents of frame
-              clearFrame (availableFrame);
+                clearFrame (availableFrame);
             }
 
-          if (maxFrameNum >= NUM_FRAMES - 1)
-            {
-              PMevict (evictPageFromFrameNum, pageToEvict);
-              removeReference (parentOfEvictPageFromFrameNum, evictPageFromFrameNum);
-              availableFrame = evictPageFromFrameNum;
-              clearFrame (availableFrame);
-            }
-
-          // point to new page table from its parent
-          PMwrite (oldAddress * PAGE_SIZE + levelOffset, availableFrame);
-          curAddress = availableFrame;
+            // point to new page table from its parent
+            PMwrite (oldAddress * PAGE_SIZE + levelOffset, availableFrame);
+            curAddress = availableFrame;
         }
     }
 
-  PMrestore (curAddress, virtualPageNum);
-  return curAddress;
+    PMrestore (curAddress, virtualPageNum);
+    return curAddress;
 }
 
 /* Reads a word from the given virtual address
@@ -238,13 +249,15 @@ word_t getAddress (uint64_t virtualAddress)
  */
 int VMread (uint64_t virtualAddress, word_t *value)
 {
-  if (VIRTUAL_MEMORY_SIZE < virtualAddress) return 0;
+    if (VIRTUAL_MEMORY_SIZE <= virtualAddress) {
+        return 0;
+    }
 
-  word_t curAddress = getAddress (virtualAddress);
+    word_t curAddress = getAddress (virtualAddress);
 
-  PMread (curAddress * PAGE_SIZE + getOffset (virtualAddress), value);
+    PMread (curAddress * PAGE_SIZE + getOffset (virtualAddress), value);
 
-  return 1;
+    return 1;
 }
 /* Writes a word to the given virtual address.
  *
@@ -254,10 +267,13 @@ int VMread (uint64_t virtualAddress, word_t *value)
  */
 int VMwrite (uint64_t virtualAddress, word_t value)
 {
-  if (VIRTUAL_MEMORY_SIZE < virtualAddress) return 0;
 
-  word_t curAddress = getAddress (virtualAddress);
-  PMwrite (curAddress * PAGE_SIZE + getOffset (virtualAddress), value);
+    if (VIRTUAL_MEMORY_SIZE <= virtualAddress) {
+        return 0;
+    }
 
-  return 1;
+    word_t curAddress = getAddress (virtualAddress);
+    PMwrite (curAddress * PAGE_SIZE + getOffset (virtualAddress), value);
+
+    return 1;
 }
